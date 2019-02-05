1ccaf6d7371bdc6d7ea70023bac7f02655de2dec
Tarun Dhiman <tdhiman@worldquant.com>
2019-02-05 12:40:00 +0530

    complate addition of code foe bse data from Quandl

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ This module will download base data from quandl
 """
 
 import os
+import json
 import source
 import zipfile
 import StringIO
@@ -19,29 +20,55 @@ class Quandl:
         """
         self._url = source.QUANDL
         self._api = Api(self._url)
-        self.symbol = kwargs['symbol']
 
     def bse_metadata(self):
         """
 
         :return:
         """
-        self._url = source.BSE + '/metadata?api_key=' + source.QUANDL_API_KEY
+        self._url = source.BSE_META + '/metadata?api_key=' + source.QUANDL_API_KEY
 
         data = self._api.apply(False, url=self._url)
-        zipfile.ZipFile(StringIO.StringIO(data[0])).extractall(os.path.join(os.path.dirname(__file__), 'tmp'))
+        zipfile.ZipFile(StringIO.StringIO(data[0])).extractall(os.path.join(os.path.dirname(__file__), 'meta'))
 
         if data[0]:
-            return pd.read_csv(os.path.join(os.path.dirname(__file__), 'tmp') + '/BSE_metadata.csv')
+            return pd.read_csv(os.path.join(os.path.dirname(__file__), 'meta') + '/BSE_metadata.csv')
         else:
             raise BaseException
 
-    def bse_data(self):
+    def bse_id(self, symbol=None):
         """
 
+        :param symbol:
         :return:
         """
-        raise NotImplementedError
+        if symbol:
+            self._url = source.BSE_ID + '/BSE/' + symbol + '/metadata'
+            data = self._api.apply(False, url=self._url)
+
+            if data[0]:
+                return json.loads(data[0])['dataset']['id']
+            else:
+                return None
+        else:
+            return None
+
+    def bse_data(self, bse_id=None):
+        """
+
+        :param bse_id:
+        :return:
+        """
+        if bse_id:
+            self._url = source.BSE_ID + '/' + str(bse_id) + '/data'
+            data = self._api.apply(False, url=self._url)
+
+            if data[0]:
+                return data[0]
+            else:
+                return None
+        else:
+            return None
 
     def bse_instrument(self):
         """
--- api/source.py
+++ api/source.py
@@ -1,3 +1,4 @@
 QUANDL = "https://www.quandl.com/api/v3/datasets/WIKI/"
-BSE = "https://www.quandl.com/api/v3/databases/BSE"
+BSE_META = "https://www.quandl.com/api/v3/databases/BSE"
+BSE_ID = "https://www.quandl.com/api/v3/datasets"
 QUANDL_API_KEY = "kXzs8KXhFUFWf6-q7so9"
